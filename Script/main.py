9c3ddb26630fdf4b14eb8aca0a3063ccf6adc10d
Luciano Guzman <lucianoguzman24@gmail.com>
2022-03-24 14:34:03 -0300

    .

--- Script/main.py
+++ Script/main.py
@@ -1,13 +1,24 @@
-from  ui.main_ui import *
+from  main_ui import *
+
+
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
-        self.setupUi()
+        self.setupUi(self)
+        self.btnAceptar.clicked.connect(aceptar)
+        self.btnCancelar.clicked.connect(cancelar)
 
-if __name__ == "__main__":
+def aceptar():
+    print("acepto")
+
+def cancelar():
+    print("cancelar")
+
+def open():
     app = QtWidgets.QApplication([])
     window = MainWindow()
     window.show()
     app.exec_()
 
+open()
