a8a1922b69d89b0a8cb6a4c88cc88a684b4bbf4c
Luciano Guzman <lucianoguzman24@gmail.com>
2022-03-24 12:39:30 -0300

    .

--- Script/main.py
+++ Script/main.py
@@ -3,9 +3,11 @@ from  ui.main_ui import *
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
-        self.setupUi(self)
+        self.setupUi()
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
     window.show()
     app.exec_()
+
